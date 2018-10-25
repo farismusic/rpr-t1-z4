bda620e07a86660dad3fbd0a6651c02c76021ad1
farismusic <44246682+farismusic@users.noreply.github.com>
2018-10-25 11:23:14 +0200

    Problemi

---
+++ test/ba/unsa/etf/rpr/predavanje03/StudentTest.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr.predavanje03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getIme() {
+    }
+
+    @Test
+    void setIme() {
+    }
+
+    @Test
+    void getPrezime() {
+    }
+
+    @Test
+    void setPrezime() {
+    }
+
+    @Test
+    void getBrojIndeksa() {
+    }
+
+    @Test
+    void setBrojIndeksa() {
+    }
+
+    @Test
+    void ispisi() {
+    }
+}
